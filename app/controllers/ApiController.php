c0fa863eef6f044bd1c804e11613f57c8e64311b
xiaoyunqiang <my455628442@163.com>
2016-02-09 11:52:33 +0800

    zs
    zs

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -12,7 +12,6 @@ class ApiController extends ControllerBase
         if (!$this->request->isPost()) {
             $function = $_GET['function'];
             $function_mean = explode('/', $function);
-            var_dump($function_mean);exit;
             $reflectionMethod = new ReflectionMethod($function_mean[0], $function_mean[1]);
             $reflectionMethod->invoke(new $function_mean[0]);
             die();
