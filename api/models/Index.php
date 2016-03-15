5f5e6dffabad235387d8aa93ff5a46da84b4fd56
xiaoyunqiang <my455628442@163.com>
2016-03-15 17:43:41 +0800

    api
    api

--- api/models/Index.php
+++ api/models/Index.php
@@ -21,8 +21,14 @@ class Index extends Base
         $all_pic = $paginator->getPaginate();
         $data['status'] = '100200';
         $data['data']['page'] = $all_pic-> current. '/' . $all_pic-> total_pages;
-        $app_auth = $_SESSION['app_auth'];
-//        var_dump($app_auth);
+        $login_user_id = intval($_GET['user_id']);
+        $login_user_collect = MeiuiUserTag::find('user_id='.$login_user_id);
+        $login_collect_tag = [];
+        if (count($login_user_collect) > 0) {
+            foreach($login_user_collect as $collect){
+                $login_collect_tag[] = $collect->tag_id;
+            }
+        }
         foreach($all_pic-> items as $value){
             $user = MeiuiUser::findFirst('id='.$value->create_user);
             $tags = MeiuiPicLinkTag::find('pic_id='.$value->id);
@@ -30,7 +36,7 @@ class Index extends Base
             $user_tag = [];
             if (count($tags) > 0) {
                 foreach($tags as $v){
-                    if($v->user_id == $app_auth['app_user_id']){
+                    if(in_array($v->tag_id, $login_collect_tag)){
                         $user_tag[] = $v-> tag_name ;
                     } else {
                         $sys_tag[] = $v-> tag_name ;
@@ -52,29 +58,6 @@ class Index extends Base
                 'user_tag' => $user_tag,
             );
         }
-        die(json_encode($data));
-    }
-    public function index_data(){
-        $data = $this->main;
-        $data['status'] = '100200';
-        $data['data']['page'] = '1/15';
-        for ($i=0;$i<10;$i++){
-            $data['data']['items'][] = array(
-                'pic' => 'http://7nar8n.com1.z0.glb.clouddn.com/%E6%9E%81%E7%AE%80%EF%BC%8C%E5%A4%B4%E5%83%8F%E8%AE%BE%E7%BD%AE.jpg',
-                'pic_h' => '1334',
-                'pic_w' => '750',
-                'app_id' => '520',
-                'user_id' => '007',
-                'user_name' => '小李',
-                'user_pic' => 'http://7nar8n.com1.z0.glb.clouddn.com/11.png%3Ab7tnvih4dzs7gq5uq9gi',
-                'app_name' => 'aaa',
-                'brief' => '这是一个短很长很长的简介',
-                'classification' => '标签,个人资料'
-            );
-
-        }
-        $a = 'ae7b155ebd33acb4e69b70446c800d91';
-        $data['alert']['msg'] = $this->lang['request_success'];
         die(json_encode($data));
     }
 }
