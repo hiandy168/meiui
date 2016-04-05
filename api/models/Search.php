462356301ff46a02729b01a630f6cce5c88823d8
xiaoyunqiang <my455628442@163.com>
2016-04-05 22:13:33 +0800

    search
    search

--- api/models/Search.php
+++ api/models/Search.php
@@ -43,7 +43,14 @@ class Search extends Base
         $all_pic = $paginator->getPaginate();
         $data['status'] = '200200';
         $data['data']['page'] = $all_pic-> current. '/' . $all_pic-> total_pages;
-        $app_auth = $_SESSION['app_auth'];
+        $login_user_id = intval($_GET['user_id']);
+        $login_user_collect = MeiuiUserTag::find('user_id='.$login_user_id);
+        $login_collect_tag = [];
+        if (count($login_user_collect) > 0) {
+            foreach($login_user_collect as $collect){
+                $login_collect_tag[] = $collect->tag_id;
+            }
+        }
         foreach($all_pic-> items as $pic_value){
             $pic = MeiuiPic::findFirst('id='.$pic_value->pic_id);
             if($pic){
@@ -53,9 +60,9 @@ class Search extends Base
                 $user_tag = [];
                 if (count($tags) > 0) {
                     foreach($tags as $v){
-                        if($v->user_id == $app_auth['app_user_id']){
+                        if(in_array($v->tag_id, $login_collect_tag)){
                             $user_tag[] = $v-> tag_name ;
-                        } else {
+                        } else if($v->tag_type == 2){
                             $sys_tag[] = $v-> tag_name ;
                         }
                     }
