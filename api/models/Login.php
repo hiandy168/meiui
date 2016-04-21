b2bc497c91c76010761ae49a6a4a40d93e39f428
xiaoyunqiang <my455628442@163.com>
2016-04-21 16:31:44 +0800

    找回密码
    找回密码

--- api/models/Login.php
+++ api/models/Login.php
@@ -196,4 +196,41 @@ class Login extends Base
         }
         die(json_encode($data));
     }
+
+    public function pwd_back(){
+        $data = $this->main;
+        if(!empty($_GET['username']) and !empty($_GET['password'])){
+            $conditions = " username = :username:";
+            $parameters = array(
+                "username" => $_GET['username'],
+            );
+            $user = MeiuiUser::findFirst(array(
+                $conditions,
+                "bind" => $parameters
+            ));
+            if(!$user){
+                $data['status'] = $this-> status['save_user_error'];
+                $data['data'] = array();
+                $data['alert']['msg'] = $this-> lang['save_user_error'];
+                die(json_encode($data));
+            } else {
+                $user->password = sha1($_GET['password']);
+                $user->save();
+            }
+            $data['status'] = '500200';
+            $data['data'] = array(
+                'user_id' => $user->id,
+                'user_name' => $user->nickname,
+                'user_pic' => $user->user_pic,
+            );
+            $data['alert']['msg'] = $this->lang['request_success'];
+            die(json_encode($data));
+        } else {
+            $data['status'] = $this -> status['lack_user_info'];
+            $data['data'] = array();
+            $data['alert']['msg'] = $this->lang['lack_user_info'];
+            die(json_encode($data));
+        }
+
+    }
 }
