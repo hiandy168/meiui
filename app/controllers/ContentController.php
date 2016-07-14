89baa5b5b09c46281c82212841a51892c0808197
xiaoyunqiang <my455628442@163.com>
2016-07-14 15:50:30 +0800

    add pic
    add pic

--- app/controllers/ContentController.php
+++ app/controllers/ContentController.php
@@ -29,7 +29,109 @@ class ContentController extends ControllerBase
         }
     }
     public function addAction(){
+        if($_POST){
+            $img_url = 'http://img.meiui.me' . $_POST['img_url'];
+            if($img_url){
+                $array_url  = explode('/app/', $img_url);
+                $img_value = explode('/', $array_url[1]);
+                $url_value = getimagesize($img_url);
+                if($url_value){
+                    $this->insert_data($img_value[0], $img_value[1], $url_value);
+                }
+            }
+        }else{
 
+        }
+    }
+
+    public function insert_data($app, $file, $file_info){
+        //先判断APP 是否存在，不存在新建APP
+        $conditions = " app_name = :app_name: ";
+        $parameters = array(
+            "app_name" => $app,
+        );
+        $db_app = MeiuiAPP::findFirst(array(
+            $conditions,
+            "bind" => $parameters
+        ));
+        if(!$db_app){
+            $db_app = new MeiuiApp();
+            $db_app-> app_name = $app;
+            $db_app-> create_user = $_SESSION['auth']['id'];
+            $db_app-> create_time = time();
+            $db_app->save();
+        }
+        //再判断PIC 是否存在，不存在新建PIC
+        $tags = explode('.', $file);
+        $pic_url = 'http://img.meiui.me' . $_POST['img_url'];
+
+        $conditions = " pic_url = :pic_url: ";
+        $parameters = array(
+            "pic_url" => $pic_url,
+        );
+        $db_pic = MeiuiPic::findFirst(array(
+            $conditions,
+            "bind" => $parameters
+        ));
+        if(!$db_pic){
+            $db_pic = new MeiuiPic();
+            $db_pic-> app_id = $db_app->id;
+            $db_pic-> app_name = $db_app->app_name;
+            $db_pic-> pic_url = $pic_url;
+            $db_pic-> pic_w = $file_info[0];
+            $db_pic-> pic_h = $file_info[1];
+            $db_pic-> brief = 'brief default tag  ' . $app . ',' . $tags[0] ;
+            $db_pic-> create_user = $_SESSION['auth']['id'];
+            $db_pic-> create_time = time();
+            $db_pic->save();
+        }
+        // 判断标签 不存在则插入
+        $all_tag = explode('，',$tags[0]);
+        $all_tag[] = $app;
+        foreach($all_tag as $one_tag){
+            $this-> insert_tag_link_pic($one_tag, $db_pic->id);
+        }
+    }
+
+    public function insert_tag($tag){
+        $conditions = " tag_name = :tag_name:";
+        $parameters = array(
+            "tag_name" => $tag,
+        );
+        $db_tag = MeiuiTag::findFirst(array(
+            $conditions,
+            "bind" => $parameters
+        ));
+        if(!$db_tag){
+            $db_tag = new MeiuiTag();
+            $db_tag-> tag_name = $tag;
+            $db_tag-> create_user = $_SESSION['auth']['id'];
+            $db_tag-> create_time = time();
+            $db_tag->save();
+        }
+        return $db_tag;
+    }
+
+    public function insert_tag_link_pic($tag_name, $pic_id){
+        $tag = $this->insert_tag($tag_name);
+        $conditions = " tag_name = :tag_name: and pic_id =:pic_id:";
+        $parameters = array(
+            "tag_name" => $tag->tag_name,
+            "pic_id" => $pic_id,
+        );
+        $db_tag_link_pic = MeiuiPicLinkTag::findFirst(array(
+            $conditions,
+            "bind" => $parameters
+        ));
+        if(!$db_tag_link_pic){
+            $db_tag_link_pic = new MeiuiPicLinkTag();
+            $db_tag_link_pic-> pic_id = $pic_id;
+            $db_tag_link_pic-> user_id = $_SESSION['auth']['id'];
+            $db_tag_link_pic-> tag_id = $tag->id;
+            $db_tag_link_pic-> tag_name = $tag->tag_name;
+            $db_tag_link_pic->save();
+        }
+        return $db_tag_link_pic;
     }
 
 }
--- app/views/content/add.volt
+++ app/views/content/add.volt
@@ -2,10 +2,13 @@
 
 <div align="center">
     {{ content() }}
-
+{{ form("content/add") }}
     <ul class="pager">
         <li class="previous pull-left">
             {{ link_to("content/view", "&larr; 返回") }}
+        </li>
+        <li class="pull-right">
+            {{ submit_button("保存", "class": "btn btn-success") }}
         </li>
     </ul>
 
@@ -13,13 +16,10 @@
         <h2> 内容添加 </h2>
     </div>
     <div class="page-header">
-        <input class="form-control" style="float:left;" value=""" />
+        <input class="form-control" style="float:left;" value="" name = "img_url" />
     </div>
     <div class="page-header">
         <h4> 粘贴图片URL，URL 格式需要是可调用的URL比如：http://img.meiui.me/app/AirVisual/空气质量，列表，卡片式.PNG </h4>
     </div>
-
-
-    <script>
-
+</form>
 </div>
