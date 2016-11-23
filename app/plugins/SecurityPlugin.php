bd0d4be0aad31c5fa4646ce90a075529a1f4d6cb
xiaoyunqiang <xiaoyunqiang@laoyuegou.com>
2016-11-23 14:59:40 +0800

    user_list

--- app/plugins/SecurityPlugin.php
+++ app/plugins/SecurityPlugin.php
@@ -42,7 +42,7 @@ class SecurityPlugin extends Plugin
                 'user'              => array('register', 'list','delete'),
                 'tag'               => array('list', 'create','delete'),
                 'content'               => array('view', 'add'),
-                'pic'               => array('list', 'create','delete','changeBrief'),
+                'pic'               => array('list', 'user_list', 'create','delete','changeBrief'),
                 'search'            => array('list', 'create','delete','userSearchList'),
                 'feedback'          => array('list', 'view'),
                 'app'               => array('list','unpass'),
