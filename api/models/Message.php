48ff4b30017179b1060ceeb0d01498c83062fee7
xiaoyunqiang <xiaoyunqiang@laoyuegou.com>
2017-03-20 10:19:04 +0800

    add pic

--- api/models/Message.php
+++ api/models/Message.php
@@ -36,7 +36,7 @@ class Message extends Base
             $data['data']['items'][] = array(
                 'msg' => $item->msg,
                 'msg_flag' => $item->msg_flag,
-                'msg_pic' => $item->pic,
+                'msg_pic' => (string)$item->pic,
                 'created_at' => $item->created_at,
             );
         }
